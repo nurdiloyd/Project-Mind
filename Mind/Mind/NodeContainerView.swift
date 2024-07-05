a380b161bb1720ee05b9ca46aa9958d61398a816
Nurdoğan Karaman <nurdogankaraman@hotmail.com>
2024-07-04 20:07:22 -0700

    Update codes.

--- Mind/Mind/NodeContainerView.swift
+++ Mind/Mind/NodeContainerView.swift
@@ -208,14 +208,3 @@ struct NodeContainerView: View {
         }
     }
 }
-
-
-extension View {
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, content: (Self) -> Content) -> some View {
-        if condition {
-            content(self)
-        } else {
-            self
-        }
-    }
-}
--- Mind/Mind/ViewExtensions.swift
+++ Mind/Mind/ViewExtensions.swift
@@ -16,3 +16,13 @@ extension View {
     .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
   }
 }
+
+extension View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, content: (Self) -> Content) -> some View {
+        if condition {
+            content(self)
+        } else {
+            self
+        }
+    }
+}
