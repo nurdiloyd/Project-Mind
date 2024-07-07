75f4fc4d579fb9719f75d7523cde1194571f064a
Nurdoğan Karaman <nurdogankaraman@hotmail.com>
2024-07-06 20:43:32 -0700

    Add position reader extension.

--- Mind/Mind/Helpers/Extensions/ViewExtensions.swift
+++ Mind/Mind/Helpers/Extensions/ViewExtensions.swift
@@ -17,6 +17,23 @@ extension View {
   }
 }
 
+struct PositionPreferenceKey: PreferenceKey {
+  static var defaultValue: CGPoint = .zero
+  static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
+}
+
+extension View {
+  func readPosition(onChange: @escaping (CGPoint) -> Void) -> some View {
+    background(
+      GeometryReader { geometryProxy in
+        Color.blue
+              .preference(key: PositionPreferenceKey.self, value: geometryProxy.frame(in: .global).origin)
+      }
+    )
+    .onPreferenceChange(PositionPreferenceKey.self, perform: onChange)
+  }
+}
+
 extension View {
     @ViewBuilder func `if`<Content: View>(_ condition: Bool, content: (Self) -> Content) -> some View {
         if condition {
