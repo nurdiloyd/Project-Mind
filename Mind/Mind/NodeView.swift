dd06ea573cff432d0a4e385e7e551a5bf19fbad5
Nurdoğan Karaman <nurdogankaraman@hotmail.com>
2024-07-10 17:09:24 -0700

    Update textField behaviour of nodeView.

--- Mind/Mind/NodeView.swift
+++ Mind/Mind/NodeView.swift
@@ -6,6 +6,7 @@ struct NodeView: View {
     var setTitle: (String) -> Void
     
     @State private var isEditing: Bool = false
+    @FocusState private var isFocus: Bool
     @State var inputText: String = ""
 
     public static let titleHeight: CGFloat = 30
@@ -18,39 +19,50 @@ struct NodeView: View {
     }
     
     var body: some View {
-            VStack(spacing: 0) {
-                if isEditing {
-                    TextField("Node Title", text: $inputText, onCommit: {
+        VStack(spacing: 0) {
+            if isEditing
+            {
+                TextField("Node Title", text: $inputText, onEditingChanged: { isStart in
+                        if !isStart
+                        {
+                            isEditing = false
+                            setTitle(inputText)
+                        }
+                    })
+                    .focused($isFocus)
+                    .onSubmit {
                         isEditing = false
                         setTitle(inputText)
-                    })
+                    }
                     .foregroundColor(Color(NSColor.windowFrameTextColor))
                     .multilineTextAlignment(.center)
                     .textFieldStyle(PlainTextFieldStyle())
                     .frame(height: NodeView.titleHeight)
-                } else {
-                    Text("\(node.order) \(node.title)")
-                        .foregroundColor(Color(NSColor.windowFrameTextColor))
-                        .font(.headline)
-                        .frame(height: NodeView.titleHeight)
-                        .onTapGesture(count: 1) {
-                            inputText = node.title
-                            isEditing = true
-                        }
-                }
-                
-                if let customImage = image {
-                    Image(nsImage: customImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: imageHeight, height: imageHeight)
-                        .clipped()
-                        .contentShape(Rectangle())
-                }
             }
-            .frame(maxWidth: .infinity)
-            .background(Color(NSColor.windowBackgroundColor))
-            .cornerRadius(11)
-            .shadow(radius: 8)
+            else {
+                Text("\(node.order) \(node.title)")
+                    .foregroundColor(Color(NSColor.windowFrameTextColor))
+                    .font(.headline)
+                    .frame(height: NodeView.titleHeight)
+                    .onTapGesture(count: 1) {
+                        inputText = node.title
+                        isEditing = true
+                        isFocus.toggle()
+                    }
+            }
+            
+            if let customImage = image {
+                Image(nsImage: customImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: imageHeight, height: imageHeight)
+                    .clipped()
+                    .contentShape(Rectangle())
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color(NSColor.windowBackgroundColor))
+        .cornerRadius(11)
+        .shadow(radius: 8)
     }
 }
