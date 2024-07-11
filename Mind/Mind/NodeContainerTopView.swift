03c411333351b6f4510ae7a725fe71dc4ec98bc2
Nurdoğan Karaman <nurdogankaraman@hotmail.com>
2024-07-11 01:33:12 -0700

    Update photoPicker button.

--- Mind/Mind/NodeContainerTopView.swift
+++ Mind/Mind/NodeContainerTopView.swift
@@ -5,27 +5,31 @@ struct NodeContainerTopView: View {
     var importImage: (Data) -> Void
     var deleteImage: () -> Void
     var addChildNode: () -> Void
-    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var selectedImage: PhotosPickerItem? = nil
     @Binding var hasImage: Bool
     public static var height: CGFloat = 30
-    
+    @State private var isPickerPresented: Bool = false
+
     var body: some View {
         HStack {
-            PhotosPicker(
-                selection: $selectedItem,
-                matching: .images,
-                photoLibrary: .shared()
-            ) {
+            Button(action: {
+                isPickerPresented = true
+            }) {
                 Image(systemName: "plus.circle")
                     .bold()
                     .frame(maxHeight: .infinity)
             }
+            .photosPicker(isPresented: $isPickerPresented,
+                          selection: $selectedImage,
+                          matching: .images,
+                          photoLibrary: .shared())
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
-            .onChange(of: selectedItem) { _, newItem in
-                if let newItem = newItem {
+            .onChange(of: selectedImage) { _, newItem in
+                print("dsdsds")
+                if let item = newItem {
                     Task {
-                        if let data = try? await newItem.loadTransferable(type: Data.self) {
+                        if let data = try? await item.loadTransferable(type: Data.self) {
                             importImage(data)
                             hasImage = true
                         }
