cf37fcc9d8d2911082f1f42fc3886aa859876d42
Nurdoğan Karaman <nurdogankaraman@hotmail.com>
2024-08-15 21:26:36 -0700

    Differantiate the database locations between development and release.

--- Mind/Mind/Helpers/FileHelper.swift
+++ Mind/Mind/Helpers/FileHelper.swift
@@ -1,9 +1,41 @@
 import Foundation
 
 struct FileHelper {
+    #if DEBUG
+    private static let environment = "dev"
+    #else
+    private static let environment = "release"
+    #endif
+    
+    static func getSavingDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let appPath = path.appendingPathComponent("app_data_\(environment)")
+        createDirectoryIfNeeded(at: appPath)
+        
+        return appPath
+    }
+    
+    static func getImagesDirectory() -> URL {
+        let path = getSavingDirectory().appendingPathComponent("images")
+        createDirectoryIfNeeded(at: path)
+        
+        return path
+    }
+        
+    private static func createDirectoryIfNeeded(at path: URL) {
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: path.path) {
+            do {
+                try fileManager.createDirectory(at: path, withIntermediateDirectories: true, attributes: nil)
+                print("Directory created at \(path.path)")
+            } catch {
+                print("Error creating directory: \(error.localizedDescription)")
+            }
+        }
+    }
+
     static func saveImage(data: Data, filename: String) {
-        let fileURL = getDocumentsDirectory().appendingPathComponent(filename)
-        createDocumentsDirectoryIfNeeded()  // Ensure the directory exists
+        let fileURL = getImagesDirectory().appendingPathComponent(filename)
         do {
             try data.write(to: fileURL)
             print("Image saved \(filename)")
@@ -18,7 +50,7 @@ struct FileHelper {
                 return nil
             }
             
-            let fileURL = getDocumentsDirectory().appendingPathComponent(name)
+            let fileURL = getImagesDirectory().appendingPathComponent(name)
             do {
                 let data = try Data(contentsOf: fileURL)
                 print("Image loaded \(name)")
@@ -38,30 +70,12 @@ struct FileHelper {
                return
             }
             
-            let fileURL = getDocumentsDirectory().appendingPathComponent(name)
+            let fileURL = getImagesDirectory().appendingPathComponent(name)
             do {
                 try FileManager.default.removeItem(at: fileURL)
                 print("Image deleted \(fileURL.path)")
             } catch {
                 print("Image is not deleted: \(error.localizedDescription)")
-            }
-        }
-    }
-
-    private static func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-
-    private static func createDocumentsDirectoryIfNeeded() {
-        let fileManager = FileManager.default
-        let documentsDirectory = getDocumentsDirectory()
-        if !fileManager.fileExists(atPath: documentsDirectory.path) {
-            do {
-                try fileManager.createDirectory(at: documentsDirectory, withIntermediateDirectories: true, attributes: nil)
-                print("Documents directory created at \(documentsDirectory.path)")
-            } catch {
-                print("Error creating documents directory: \(error.localizedDescription)")
             }
         }
     }
--- Mind/Mind/MindApp.swift
+++ Mind/Mind/MindApp.swift
@@ -10,17 +10,12 @@ import SwiftData
 
 @main
 struct MindApp: App {
-    var body: some Scene
-    {
-        WindowGroup
-        {
+    var body: some Scene {
+        WindowGroup {
             ContentView()
-                .onAppear
-                {
-                    if let window = NSApplication.shared.windows.first
-                    {
-                        if let screenSize = NSScreen.main?.frame.size
-                        {
+                .onAppear {
+                    if let window = NSApplication.shared.windows.first {
+                        if let screenSize = NSScreen.main?.frame.size {
                             let halfScreenSize = NSSize(width: screenSize.width / 2, height: screenSize.height / 2)
                             window.setContentSize(halfScreenSize)
                             window.setFrame(NSRect(x: 0, y: 0, width: halfScreenSize.width, height: halfScreenSize.height), display: true)
@@ -32,7 +27,14 @@ struct MindApp: App {
                         }
                     }
                 }
+                .modelContainer(try! setupModelContainer())
         }
-        .modelContainer(for: [BoardData.self, NodeData.self])
+    }
+    
+    func setupModelContainer() throws -> ModelContainer {
+        let containerURL = FileHelper.getSavingDirectory().appendingPathComponent("user")
+        let configuration = ModelConfiguration(url: containerURL)
+        let container = try ModelContainer(for: BoardData.self, NodeData.self, configurations: configuration)
+        return container
     }
 }
